63b930e864e4cb762b746fb5d951a518d018f457
oyegorov <yegorov.oleg@gmail.com>
2015-12-01 21:30:45 -0500

    GUI menu

---
+++ Code/SotiWorld/Assets/Scripts/CastleEntranceCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CastleEntranceCollider : MonoBehaviour {
+    public GameObject menu;
+    public GameObject fps;
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider collider)
+	{
+	    if (collider.tag == "Player")
+	    {
+            menu.SetActive(true);
+
+            fps.GetComponent<FirstPersonController>().enabled = false;
+        }
+	}
+}
---
+++ Code/SotiWorld/Assets/Scripts/CastleGameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastleGameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void Restartlevel()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+
+    public void GoToMaze()
+    {
+        Application.LoadLevel("labyrinth");
+    }
+}
