767acc76faf746691676833e45af17ab9b154685
oyegorov <yegorov.oleg@gmail.com>
2015-12-08 19:56:12 -0500

    Device management collider: in progress

--- Code/SotiWorld/Assets/Scripts/CharacterCollider.cs
+++ Code/SotiWorld/Assets/Scripts/CharacterCollider.cs
@@ -1,8 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class CharacterCollider : MonoBehaviour {
+public class CharacterCollider : MonoBehaviour
+{
+    public GameObject manageDevicePanel;
+    public GameObject manageDevicePrompt;
+
     void OnTriggerEnter(Collider other)
     {
+        manageDevicePrompt.GetComponent<Text>().text = String.Format("Press M to manage device '{0}'", other.name);
+        manageDevicePanel.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        manageDevicePanel.SetActive(false);
     }
 }
